d2e62fe434f2ce683aac9295b9700cb84769589c
David Menne <david.menne@gmail.com>
2020-04-05 00:00:56 +0200

    Multiple File upload working.

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -1,41 +1,57 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Validators, AbstractControl } from '@angular/forms';
+
+
 
 // import { ImageData } from './ImageModel';
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/xml',
+    'Authorization': 'jwt-token'
+  })
+};
 
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.css']
+  styleUrls: ['./create-product.component.css'],
 })
 
 
 export class CreateProductComponent implements OnInit {
-  title = 'ImageUploaderFrontEnd';
+  heading = 'ImageUploaderFrontEnd';
   url = '/image/upload';
   uploadedFiles: any[] = [];
+  title = new FormControl('');
 
 
   constructor(private httpClient: HttpClient) { }
   ngOnInit() {}
 
   myUploader(event):void{
+    console.log('My Email upload',this.title.value);
     console.log('My File upload',event);
+    console.log("Variable name: ",this.title.value);
     if(event.files.length == 0){
       console.log('No file selected.');
       return;
     }
-    var fileToUpload = event.files[0];
-    let input = new FormData();
-    input.append("myFile", fileToUpload);
-
-    this.httpClient
-    .post(environment.baseUrl + this.url, input)
-    .subscribe(res => {
-      console.log(res);
-    });
+    for (let j = 0; j < event.files.length; j++) {
+      let data = new FormData();
+      let fileItem = event.files[j];
+      console.log(fileItem.name);
+      data.append('image', fileItem);
+      data.append( 'email', this.title.value);
+      this.httpClient
+      .post(environment.baseUrl + this.url, data)
+      .subscribe(res => {
+        console.log(res);
+      });
+    }
   }
 
   // upload completed event
